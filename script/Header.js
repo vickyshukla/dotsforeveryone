fe208251dbdf5dad3ef31403779e7cdbd095542c
krunaldevtale <krunaldevtale79@gmail.com>
2024-07-25 12:05:02 +0530

    add taskbar hover

--- script/Header.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('mousemove', function (event) {
-    const navbar = document.querySelector('.navbar-slide');
-
-    if (event.clientY <= 50) {
-        navbar.classList.add('show');
-    } else {
-        navbar.classList.remove('show');
-    }
-});
