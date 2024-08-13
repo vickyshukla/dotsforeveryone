d7bd3e6c9f4cfc6e3279d675a13a6e2b4b86e3c1
krunaldevtale <krunaldevtale79@gmail.com>
2024-08-13 12:44:48 +0530

    add pinned functionaliy in taskbar

--- script/taskbar.js
+++ script/taskbar.js
@@ -1,9 +1,36 @@
-document.addEventListener('mousemove', function (event) {
+document.addEventListener('DOMContentLoaded', function () {
+    const pinIcon = document.getElementById('pinned');
     const taskbar = document.querySelector('.taskbar-slide');
+    let isPinned = false;
 
-    if (event.clientY <= 50) {
-        taskbar.classList.add('show');
-    } else {
-        taskbar.classList.remove('show');
+
+    pinIcon.addEventListener('click', function () {
+        if (isPinned) {
+           
+            pinIcon.classList.add('ri-pushpin-line');
+            pinIcon.classList.remove('ri-unpin-line');
+            isPinned = false;
+            // Allow hover effect
+            document.addEventListener('mousemove', handleMouseMove);
+        } else {
+            // Change to pinned state
+            pinIcon.classList.remove('ri-pushpin-line');
+            pinIcon.classList.add('ri-unpin-line');
+            isPinned = true;
+            // Disable hover effect
+            document.removeEventListener('mousemove', handleMouseMove);
+            // Show taskbar
+            taskbar.classList.add('show');
+        }
+    });
+
+    function handleMouseMove(event) {
+        if (event.clientY <= 50) {
+            taskbar.classList.add('show');
+        } else {
+            taskbar.classList.remove('show');
+        }
     }
+
+    document.addEventListener('mousemove', handleMouseMove);
 });
